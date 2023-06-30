cce0bc6528e3ee278ae36179a66bba21a723db3a
DanielRPTI <drjunior988@hotmail.com>
2023-06-30 10:04:10 -0300

    Post tests working and tring add DELETE

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const prisma = new PrismaClient
 
 const bodyParser = require('body-parser')
  
-// parse application/x-www-form-urlencoded
+// parse application/x-www-form-urlencoded default config
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
@@ -17,6 +17,7 @@ app.get('/', (req, res) =>{
     return res.status(201).json(task)
 })
 
+// Get all task on project 
 app.get('/task', async (req, res) => {
   const listTask = await prisma.tasks.findMany();
 
@@ -25,27 +26,78 @@ app.get('/task', async (req, res) => {
   });
 });
 
+// Created Tasks 
 app.post('/task', async (req, res) => {
+  const titleName = req.body.title;
   
-  try {
-    const data = req.body;
-    console.log(!data)
+  if (!titleName){
+    return res.status(400).json({message: 'Title is missing, please try to insert something on body'})
+  }
 
-//    const message = 'Tarefa criada com sucesso'; 
-//    await prisma.tasks.create({
-//      data: {
-//        'title' : {titleName},
-//      }
-//    });
-//    console.log('Created Task')
+  try {
+    const message = 'Task created succefully';
+    const Task = await prisma.tasks.findFirst({
+      where: { title: titleName}
+    });
+
+    if (!Task){
+      await prisma.tasks.create({
+        data: {
+          'title': titleName,
+        }
+      });
+      console.log('Created a new title, lets start');
+      return res.json({message});
+    }
     return res.json({data});
 
   } catch(e) {
     console.error(e);
-    return res.status(500).json({message: 'something wrong'});
+    return res.status(500).json({message: 'something wrong or try to insert something diferent'});
   }
-})
+});
 
-app.listen(3000,()=> {
+app.delete('/task', async (req, res) => {
+  const IdTask = req.body.id;
+  
+  if(!IdTask){
+    return res.status(400).json({message:'Id is missing, please try to insert a Id Task valid'})
+  }
+
+  try{
+    const ID = await prisma.tasks.findFirst({
+      where:{ id: IdTask},
+    });
+
+    if(!ID){
+      await prisma.tasks.delete({
+        where:{
+          id : IdTask
+        },
+      });
+      return res.json({menssage:'Task deleted succefully!'})
+    };
+    return res.json({data});
+
+  } catch(e){
+    console.error(e);
+    return res.status(500).json({message: 'Something wrong'});
+  };
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8080,()=> {
   console.log(`server running`)
 });
